f2fff268cb1e64e9711cbd108d1f131322edd6ed
VESPT <vesp1989@gmail.com>
2016-05-31 02:52:58 +0900

    Display onnyanoko

---
+++ app/src/main/java/sessue/bodychange/MainActivity.java
@@ -0,0 +1,111 @@
+package sessue.bodychange;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
+import sessue.bodychange.OpenCV.Reader;
+
+public class MainActivity extends AppCompatActivity {
+    // メンバ変数
+    private static final String TAG = "MainActivity"; // Debug TAG
+    private Reader oppyReader;
+
+    // Initialize OpenCV
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
+
+    //// ここからデフォルトの設定
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        // OpenCV
+        if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0,
+                MainActivity.this, mOpenCVCallBack)) {
+            Log.e("TEST", "Cannot connect to OpenCV Manager");
+        }
+
+        oppyReader = new Reader(this);
+        oppyReader.DisplayImage();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    ////ここまでデフォルトの設定
+    // ここからOpenCVを使った処理
+    // ライブラリ初期化完了後に呼ばれるコールバック (onManagerConnected)
+    // public abstract class BaseLoaderCallback implements LoaderCallbackInterface
+    private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    //your code
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 非同期でライブラリの読み込み/初期化を行う
+        // static boolean initAsync(String Version, Context AppContext,LoaderCallbackInterface Callback)
+        //OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_4, this, mOpenCVCallBack);
+    }
+
+
+
+}
---
+++ app/src/main/java/sessue/bodychange/OpenCV/Reader.java
@@ -0,0 +1,116 @@
+package sessue.bodychange.OpenCV;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import sessue.bodychange.R;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by vesp on 16/05/30.
+ */
+public class Reader extends Activity {
+    Activity activity;
+    public Reader(Activity inActivity){
+        activity = inActivity;
+    }
+
+
+
+    public void DisplayImage(){
+        // 画像読み込み（OpenCVでなく、アンドロイドの関数）
+        // アプリのリソースを管理するインスタンス　getResourcesはActivityのクラス
+        Resources appResource = activity.getResources();
+        Bitmap oppyBmpSrc = BitmapFactory.decodeResource(appResource, R.drawable.anime2);
+        //Bitmap oppyBmpSrc = BitmapFactory.decodeResource(appResource, R.drawable.real2);
+
+        //  画像をMatへ変換
+        Mat oppyMat = new Mat(oppyBmpSrc.getHeight(), oppyBmpSrc.getWidth(), CvType.CV_8UC1);;
+        Utils.bitmapToMat(oppyBmpSrc, oppyMat);
+
+        //  グレースケール変換
+        Imgproc.cvtColor(oppyMat, oppyMat, Imgproc.COLOR_RGB2GRAY);
+
+        //  Cannyでエッジ検出
+        Imgproc.Canny(oppyMat, oppyMat, 80, 100);
+
+        //  Bitmapに変換する前にRGB形式に変換
+        Imgproc.cvtColor(oppyMat, oppyMat, Imgproc.COLOR_GRAY2RGBA, 4);
+
+        //  Bitmap dst に空のBitmapを作成
+        Bitmap oppyBmpDst = Bitmap.createBitmap(oppyMat.width(), oppyMat.height(), Bitmap.Config.ARGB_8888);
+
+        //  MatからBitmapに変換
+        Utils.matToBitmap(oppyMat, oppyBmpDst);
+
+        //  処理をした画像を表示する
+        ImageView iv = (ImageView)activity.findViewById(R.id.ImageView01);
+        iv.setImageBitmap(oppyBmpDst);
+
+        // 画像を保存する
+        /*
+        try{
+            saveBitmap(oppyBmpDst);
+        }
+        catch(IOException e){
+            Toast.makeText(this, "画像が保存できませぬ", Toast.LENGTH_LONG).show();
+        }
+        */
+    }
+
+    // 画像を保存する
+    private void saveBitmap(Bitmap saveImage) throws IOException {
+
+        final String SAVE_DIR = "/MyPhoto/";
+        File file = new File(Environment.getExternalStorageDirectory().getPath() + SAVE_DIR);
+        try{
+            if(!file.exists()){
+                file.mkdir();
+            }
+        }catch(SecurityException e){
+            e.printStackTrace();
+            throw e;
+        }
+
+        Date mDate = new Date();
+        SimpleDateFormat fileNameDate = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String fileName = fileNameDate.format(mDate) + ".jpg";
+        String AttachName = file.getAbsolutePath() + "/" + fileName;
+
+        try {
+            FileOutputStream out = new FileOutputStream(AttachName);
+            saveImage.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        // save index
+        ContentValues values = new ContentValues();
+        ContentResolver contentResolver = activity.getContentResolver();
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.Images.Media.TITLE, fileName);
+        values.put("_data", AttachName);
+        contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
+}
